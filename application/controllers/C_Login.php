18c6dd491368e3a6b29c959f978bce742826200a
alieffebrina <alief.febrina@gmail.com>
2020-06-30 22:32:36 +0700

    template

---
+++ application/controllers/C_Login.php
@@ -0,0 +1,43 @@
+<?php 
+
+class C_Login extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_login');
+
+	}
+
+	function index(){
+		$this->load->view('v_login');
+	}
+
+	function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => $password
+			);
+		$cek = $this->m_login->cek_login("tb_user",$where)->num_rows();
+		if($cek > 0){
+
+			$data_session = array(
+				'username' => $username,
+				'nik' => $nik
+				);
+
+			$this->session->set_userdata($data_session);
+
+			redirect(base_url("C_Admin"));
+
+		}else{
+			echo "Username dan password salah !";
+		}
+	}
+
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url('C_Login'));
+	}
+}
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,128 @@
+
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <!-- <li class="header">MAIN NAVIGATION</li> -->
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Data Master</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Data User</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Jenis Customer </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Jenis Pembayaran </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Jenis Barang </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Satuan </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Barang </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Customer </a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Suplier </a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-files-o"></i> <span>Penjualan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Sales Order</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i>Delivery Order</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i>Invoice</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i>Retur</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-th"></i> <span>Pembelian</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Purchase Order</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Nota Pembayaran</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="<?php echo base_url() ?>assets/pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Pembayaran Purchase Order</a></li>
+            <li><a href="<?php echo base_url() ?>assets/pages/charts/morris.html"><i class="fa fa-circle-o"></i> Pembayaran Invoice</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Gudang</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Stok</a></li>
+            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Transfer Barang</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Accounting</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> Hutang</a></li>
+            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Piutang</a></li>
+            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Pendapatan</a></li>
+            <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Pengeluaran</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-table"></i> <span>Setting</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i>Hak Akses Login</a></li>
+            <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i>Data Kode</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
