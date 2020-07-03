b320631c79b398a19a919ce58dcba65df8a47520
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-07-04 05:30:44 +0700

    tambah menu
    tambah menu

--- application/models/M_Setting.php
+++ application/models/M_Setting.php
@@ -2,11 +2,34 @@
 
 class M_Setting extends CI_Model {
 
+    function getjenisbarang(){
+        $this->db->select('*');
+        $this->db->from('tb_jenisbarang');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    function getsatuan(){
+        $this->db->select('*');
+        $this->db->from('tb_satuan');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
 	function getprovinsi(){
 		$this->db->select('*');
 		$this->db->from('tb_provinsi');
 		$query = $this->db->get();
     	return $query->result();
+    }
+
+    function getkota($id){
+        $this->db->select('*');
+        $where = array(
+            'id_provinsi' => $id
+        );
+        $query = $this->db->get_where('tb_kota', $where);
+        return $query->result();
     }
 
     function getakses($ida){
@@ -30,6 +53,21 @@ class M_Setting extends CI_Model {
         return $query->result();
     }
 
+    function getbarang(){
+        $this->db->select('*');
+        $this->db->from('tb_jenisbarang');
+         $this->db->from('tb_satuan');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    function getsuplier(){
+        $this->db->select('*');
+        $this->db->from('tb_provinsi');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
     function getmenu1($id){
         $this->db->distinct();
         $this->db->select('id_menu, menu, icon');
@@ -40,15 +78,6 @@ class M_Setting extends CI_Model {
         );
         $query = $this->db->get_where('tb_menu', $where);
         return $query->result();
-    }
-
-    function getkota($id){
-		$this->db->select('*');
-		$where = array(
-            'id_provinsi' => $id
-        );
-        $query = $this->db->get_where('tb_kota', $where);
-    	return $query->result();
     }
 
     function edit(){
