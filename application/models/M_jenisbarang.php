1fe8bec43800d79ad92403295bf95f8570eaaa5f
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-07-04 05:23:47 +0700

    data master jenis barang

---
+++ application/models/M_jenisbarang.php
@@ -0,0 +1,51 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_jenisbarang extends CI_Model {
+
+	function getjenisbarang(){
+		$this->db->select('*');
+        $query = $this->db->get('tb_jenisbarang');
+    	return $query->result();
+    }
+
+    function cek_jenisbarang($kode){
+        $this->db->select('*');
+        $where = array(
+            'jenisbarang' => $kode
+        );
+        $query = $this->db->get_where('tb_jenisbarang', $where);
+        return $query->result();
+    }
+
+    function tambahdata(){
+        $jenisbarang = array(
+            'jenisbarang' => $this->input->post('jenisbarang')
+        );
+        
+        $this->db->insert('tb_jenisbarang', $jenisbarang);
+    }
+
+    function getspek($id){
+		$this->db->select('*');
+        $where = array(
+            'id_jenisbarang' => $id
+        );
+        $query = $this->db->get_where('tb_jenisbarang', $where);
+    	return $query->result();
+    }
+
+    function edit(){
+        $jenisbarang = array(
+            'jenisbarang' => $this->input->post('jenisbarang')
+        );
+
+        $where = array(
+            'id_jenisbarang' =>  $this->input->post('id'),
+        );
+        
+        $this->db->where($where);
+        $this->db->update('tb_jenisbarang',$jenisbarang);
+    }
+
+    
+}
