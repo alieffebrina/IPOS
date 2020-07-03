032eaa34e868cab75b9f1c7da4f9fcee6cb7fe0a
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-07-04 04:22:48 +0700

    jenisbarang
    jenisbarang

---
+++ application/views/master/v_vjenisbarang.php
@@ -0,0 +1,54 @@
+ <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Data Jenis Barang
+        <small>Lihat</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="<?php echo site_url('Welcome'); ?>"><i class="fa fa-dashboard"></i> Data Master</a></li>
+        <li><a href="<?php echo site_url('C_jenisbarang'); ?>">Data Jenis Barang</a></li>
+        <li class="active">Lihat Data Jenis Barang</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- Horizontal Form -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Lihat Data Jenis Barang</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal" method="POST" action="<?php echo site_url('C_jenisbarang')?>">
+              <div class="box-body">
+                <?php foreach ($jenisbarang as $jenisbarang) { ?>
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Nama Jenis Barang</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" id="jenisbarang" name="jenisbarang" value="<?php echo $jenisbarang->jenisbarang ?>" readonly>
+                  </div>
+                  <span id="pesan"></span>
+                </div>
+              </div>
+              <?php } ?>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                  <div class="col-sm-10">
+                    <button type="submit" class="btn btn-default">Kembali</button>
+                  </div>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+          <!-- /.box -->
+        </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
