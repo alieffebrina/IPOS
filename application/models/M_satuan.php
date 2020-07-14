602bc3491580ce6b3f9433589d5dbc42fafd9ec6
Miftakhul Hijriyah <hijriyah.vr46@gmail.com>
2020-07-15 00:57:12 +0700

    revisi model master dan transaksi

--- application/models/M_satuan.php
+++ application/models/M_satuan.php
@@ -2,41 +2,80 @@
 
 class M_satuan extends CI_Model {
 
-	function getsatuan(){
-		$this->db->select('*');
-        $query = $this->db->get('tb_satuan');
-    	return $query->result();
-    }
-
-    function cek_satuan($kode){
+    function getsatuan(){
         $this->db->select('*');
-        $where = array(
-            'satuan' => $kode
-        );
-        $query = $this->db->get_where('tb_satuan', $where);
+        //$this->db->join('tb_provinsi', 'tb_provinsi.id_provinsi = tb_pelanggan.id_provinsi');
+        //$this->db->join('tb_kota', 'tb_kota.id_kota = tb_pelanggan.id_kota');
+        $query = $this->db->get('tb_satuan');
         return $query->result();
     }
 
-    function tambahdata(){
+    function getnama($ida){
+        $where = array(
+            'id_satuan' => $ida
+        );
+        return $this->db->get_where('tb_satuan',$where)->result();
+    }
+
+    function tambahdata($id){
+        //$harga = $this->input->post('rupiah');
+        //$harga_str = preg_replace("/[^0-9]/", "", $harga);
+
         $satuan = array(
-            'satuan' => $this->input->post('satuan')
+            'id_user' => $id,
+            'satuan' => $this->input->post('satuan'),
+            //'alamat' => $this->input->post('alamat'),
+            //'id_kota' => $this->input->post('kota'),
+            //'id_provinsi' => $this->input->post('prov'),
+            //'tlp' => $this->input->post('tlp'),
+            //'limit' => $harga_str,
+            'tgl_update' => date('Y-m-d')
         );
         
         $this->db->insert('tb_satuan', $satuan);
     }
 
-    function getspek($id){
-		$this->db->select('*');
+    // function cekkodepelanggan(){
+    //     $this->db->select_max('id_pelanggan');
+    //     $idpelanggan = $this->db->get('tb_pelanggan');
+    //     return $idpelanggan->row();
+    // }
+
+    //function tambahakses($id){
+    //    $total = $this->db->count_all_results('tb_submenu');
+
+    //    for($i=0; $i<$total; $i++){
+    //        $fungsi = array('id_submenu' => $i+1 , 
+    //            'id_user' => $id);
+
+    //        $this->db->insert('tb_akses', $fungsi);            
+    //    }
+    //}
+
+    function getspek($iduser){
+        $this->db->select('*');
+        //$this->db->join('tb_provinsi', 'tb_provinsi.id_provinsi = tb_pelanggan.id_provinsi');
+        //$this->db->join('tb_kota', 'tb_kota.id_kota = tb_pelanggan.id_kota'); 
         $where = array(
-            'id_satuan' => $id
+            'id_satuan' => $iduser
         );
         $query = $this->db->get_where('tb_satuan', $where);
-    	return $query->result();
+        return $query->result();
     }
 
-    function edit(){
+    function edit($id){
+        $harga = $this->input->post('rupiah');
+        $harga_str = preg_replace("/[^0-9]/", "", $harga);
         $satuan = array(
-            'satuan' => $this->input->post('satuan')
+
+            'id_user' => $id,
+            'satuan' => $this->input->post('satuan'),
+            //'alamat' => $this->input->post('alamat'),
+            //'id_kota' => $this->input->post('kota'),
+            //'id_provinsi' => $this->input->post('prov'),
+            //'tlp' => $this->input->post('tlp'),
+            //'limit' => $harga_str,
+            'tgl_update' => date('Y-m-d')
         );
 
         $where = array(
